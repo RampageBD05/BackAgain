fc5d3121c91ea4c84e4674590b9b677b1b1a7338
RampageBD05 <113905358+RampageBD05@users.noreply.github.com>
2023-06-06 10:27:36 +0200

    Player Paddle Scripts
    PlayerPaddle is in progress. Going well so far. Got the paddle moving

---
+++ Pong Workshop2/Assets/Scripts/Paddle.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    protected Rigidbody2D rb;
+    public float speed;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+}
